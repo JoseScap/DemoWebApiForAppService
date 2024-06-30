a9115a26f3149e4353ebbe9a0a66ae78431ed129
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-06-30 12:20:22 -0300

    Add endpoints to get settings from appsettings.Development.json

--- DemoController.cs
+++ DemoController.cs
@@ -4,11 +4,35 @@ namespace DemoWebApiForAppService;
 
 [ApiController]
 [Route("api/[controller]")]
-public class DemoController
+public class DemoController(IConfiguration configuration)
 {
-    [HttpGet("HelloWorld")]
+    [HttpGet("[action]")]
     public string HelloWorld()
     {
-        return "Hello world";
+        return nameof(HelloWorld);
+    }
+
+    [HttpGet("[action]")]
+    public string SimpleValue()
+    {
+        var value = configuration.GetSection("Messages");
+        
+        return value["Value"] ?? "There is no value for [Messages:Value]";
+    }
+    
+    [HttpGet("[action]")]
+    public string DeeperValue()
+    {
+        var value = configuration.GetSection("Messages:Deep");
+        
+        return value["Value"] ?? "There is no value for [Messages:Deep:Value]";
+    }
+
+    [HttpGet("[action]")]
+    public string ConnectionString()
+    {
+        var value = configuration.GetConnectionString("CONN_STRING");
+
+        return value ?? "There is no value for connection string [CONN_STRING]";
     }
 }
